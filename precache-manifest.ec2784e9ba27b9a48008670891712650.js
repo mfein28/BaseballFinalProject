c9c3d015b282777f8f1a8e5b5e80a59dfb6bfaf0
matt.fein@summitllc.us <matt.fein@summitllc.us>
2020-07-10 17:27:01 -0400

    Updates

---
+++ precache-manifest.ec2784e9ba27b9a48008670891712650.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1a575ae50ee5b1bcba23ace6b3ad5e6d",
+    "url": "/BaseballFinalProject/index.html"
+  },
+  {
+    "revision": "1f558256959e6b93b058",
+    "url": "/BaseballFinalProject/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "f2758d6072e4267a0d86",
+    "url": "/BaseballFinalProject/static/js/2.fe7370eb.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/BaseballFinalProject/static/js/2.fe7370eb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1f558256959e6b93b058",
+    "url": "/BaseballFinalProject/static/js/main.04944b34.chunk.js"
+  },
+  {
+    "revision": "b010f1c91d3314c7830a",
+    "url": "/BaseballFinalProject/static/js/runtime-main.120618fc.js"
+  },
+  {
+    "revision": "45e92dd8bc9798124bc9fe0e8a643124",
+    "url": "/BaseballFinalProject/static/media/cardinals.45e92dd8.svg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/BaseballFinalProject/static/media/logo.5d5d9eef.svg"
+  }
+]);
